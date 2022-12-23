96506765efa9ef0d062e2f4721f24531b5b42927
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2022-12-23 14:04:41 +0800

    Added javascripts_local
    and moved the function in their own respective html to avoid messy main.js
    
    Fixed the Scroll on Top action

--- webscraper/static/js/main.js
+++ webscraper/static/js/main.js
@@ -1,18 +1,6 @@
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
 const offcanvasMenu = document.querySelector('.offcanvas-menu')
 const homearrow = document.getElementById("homearrow");
 const myDIV = document.getElementById("myDIV");
-
-openBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    offcanvasMenu.classList.add('active');
-});
-
-closeBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    offcanvasMenu.classList.remove('active');
-});
 
 function loading(){
     $("#staticLoadingModal").modal("show")
@@ -23,47 +11,27 @@ function loading(){
     $("#staticLoadingModal").modal("hide")
  });
 
- function password_show_register() {
-  var x = document.getElementById("password1");
-  var y = document.getElementById("password2");
-
-  var type = x.type === "password" ? "text" : "password";
-  x.type = type;
-  y.type = type;
-}
-
-function password_show_login() {
-
-  var z = document.getElementById("passwordForm");
-  z.type = z.type === "password" ? "text" : "password";
-}
 
 
-function password_show_account() {
-  //register page
-  var x = document.getElementById("old_password");
-  var y = document.getElementById("new_password");
-  var z = document.getElementById("confirm_new_password");
-
-  if (x.type === "password" && y.type === "password" && z.type === "password") {
-    x.type = "text";
-    y.type = "text";
-    z.type = "text";
-  } else {
-    x.type = "password";
-    y.type = "password";
-    z.type = "password";
-  }
-}
+let animationFrameId = null;
 
 window.onscroll = function () {
-  requestAnimationFrame(scrollFunction);
+  // Cancel the current animation if it's still running
+  if (animationFrameId) {
+    window.cancelAnimationFrame(animationFrameId);
+  }
+
+  // Start a new animation
+  animationFrameId = window.requestAnimationFrame(scrollFunction);
 };
 
 function scrollFunction() {
+  // Reset the animation frame ID so we can start a new animation on the next scroll event
+  animationFrameId = null;
+
   if (
-    document.body.scrollTop > 300 ||
-    document.documentElement.scrollTop > 300
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
   ) {
     homearrow.classList.add("show");
   } else {
